50bda12893f296354e318bd965e7d5cb250c9ee9
Alvin Galit <aroblesgalit@gmail.com>
2020-05-19 19:35:49 -0500

    Polished links in cards

--- src/components/ContactInfo/index.js
+++ src/components/ContactInfo/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./style.css";
-import { Link } from "react-router-dom";
 
 function ContactInfo() {
     return (
@@ -13,12 +12,12 @@ function ContactInfo() {
                 uk-icon="icon: phone"></span>714.949.1232</span>
             <span className="contactLabel uk-text-muted uk-form-label">LinkedIn</span>
             <span className="contactDesc uk-flex uk-flex-middle"><span className="contactIcon"
-                uk-icon="icon: linkedin"></span><Link to="https://www.linkedin.com/in/aroblesgalit/"
-                    target="_blank">linkedin.com/in/aroblesgalit</Link></span>
+                uk-icon="icon: linkedin"></span><a href="https://www.linkedin.com/in/aroblesgalit/"
+                    target="_blank" rel="noopener noreferrer">linkedin.com/in/aroblesgalit</a></span>
             <span className="contactLabel uk-text-muted uk-form-label">GitHub</span>
             <span className="contactDesc uk-flex uk-flex-middle"><span className="contactIcon"
-                uk-icon="icon: github"></span><Link to="https://github.com/aroblesgalit"
-                    target="_blank">github.com/aroblesgalit</Link></span>
+                uk-icon="icon: github"></span><a href="https://github.com/aroblesgalit"
+                    target="_blank" rel="noopener noreferrer">github.com/aroblesgalit</a></span>
             <hr className="uk-divider-small" />
         </div>
     );
--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import "./style.css";
 
 function PortfolioCard({ title, description, imageUrl, githubUrl, deployedUrl }) {
     return (
         <div>
-            <div className="uk-card uk-card-default">
+            <div className="portfolioCard uk-card uk-card-default">
                 <div className="uk-card-media-top">
                     <img src={imageUrl}
                         alt={title} />
@@ -12,9 +12,9 @@ function PortfolioCard({ title, description, imageUrl, githubUrl, deployedUrl })
                 <div className="uk-card-body">
                     <h3 className="uk-card-title">{title}</h3>
                     <p>{description}</p>
-                    <a className="portfolioLinks" href={githubUrl}
-                        target="_blank" rel="noopener noreferrer">GitHub Repository</a> { deployedUrl ? (<a className="portfolioLinks"
-                        href={deployedUrl} target="_blank" rel="noopener noreferrer">Deployed Link</a>) : "" }
+                    <a className="portfolioLink" href={githubUrl}
+                        target="_blank" rel="noopener noreferrer">GitHub</a> { deployedUrl ? (<a className="portfolioLink"
+                        href={deployedUrl} target="_blank" rel="noopener noreferrer">Deployed</a>) : "" }
                 </div>
             </div>
         </div>
