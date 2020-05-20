0dc63fe19211b8ccbc8cb4b7455346a3e23c7449
Alvin Galit <aroblesgalit@gmail.com>
2020-05-19 23:34:46 -0500

    Made navbar responsive

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -5,19 +5,19 @@ function ContactForm() {
     return (
         <form className="uk-form-stacked uk-position-relative" uk-height-viewport="expand: true">
             <div className="uk-margin">
-                <label className="uk-form-label uk-text-muted" for="name">Name</label>
+                <label className="uk-form-label uk-text-muted">Name</label>
                 <div className="uk-form-controls">
                     <input className="uk-input" id="name" type="text" placeholder="John Doe" />
                 </div>
             </div>
             <div className="uk-margin">
-                <label className="uk-form-label uk-text-muted" for="email">Email</label>
+                <label className="uk-form-label uk-text-muted">Email</label>
                 <div className="uk-form-controls">
                     <input className="uk-input" id="email" type="text" placeholder="johndoe@email.com" />
                 </div>
             </div>
             <div className="uk-margin">
-                <label className="uk-form-label uk-text-muted" for="message">Email</label>
+                <label className="uk-form-label uk-text-muted">Email</label>
                 <div className="uk-form-controls">
                     <textarea className="uk-textarea" id="message" rows="5"
                         placeholder="Your message..."></textarea>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import myLogo from "../../assets/images/alvinGalit_logoMark.png";
+import NavToggle from "../NavToggle";
 
 function Nav() {
+
+    const [toggleState, setToggle] = useState();
+    const [navToggle, setNavToggle] = useState();
+
+    useEffect(() => {
+        setToggle(true);
+    }, []);
+
+    function handleNavToggle() {
+        setToggle(!toggleState);
+        if (toggleState) {
+            setNavToggle(<NavToggle handleOnClick={handleOnClick} />);
+        } else {
+            setNavToggle("");
+        }
+    }
+
+    function handleOnClick() {
+        setToggle(true);
+        setNavToggle("");
+    }
+
     return (
-        <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="true">
+        <div>
+            <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="true">
 
-            <div className="uk-navbar-left">
-                <img src={myLogo} alt="My logo" className="logoMark" />
-                <Link className="uk-navbar-item uk-logo myLogo" to="/">Alvin Galit</Link>
-            </div>
+                <div className="uk-navbar-left">
+                    <img src={myLogo} alt="My logo" className="logoMark" />
+                    <Link className="uk-navbar-item uk-logo myLogo" to="/" onClick={handleOnClick}>Alvin Galit</Link>
+                </div>
 
-            <div className="uk-navbar-right">
-                <ul className="uk-navbar-nav navLink">
-                    <li><Link to="/" className="">About</Link></li>
-                    <li><Link to="/portfolio">Portfolio</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </div>
-        </nav>
+                <div className="uk-navbar-right">
+                    <ul className="uk-navbar-nav navLink">
+                        <li><Link to="/" className="">About</Link></li>
+                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                    <div className="navMenuSmall" onClick={handleNavToggle}>
+                        <span id="navMenuIcon" uk-icon="icon: menu"></span>
+                    </div>
+                </div>
+            </nav>
+            {navToggle}
+        </div>
     );
 }
 
