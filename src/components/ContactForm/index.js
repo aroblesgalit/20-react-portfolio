6c1b349b2c9c35d05637cee1c379c2c98c639a0c
Alvin Galit <aroblesgalit@gmail.com>
2020-06-11 22:07:59 -0500

    Added alerts for when message is sent successfuly and when there's missing fields

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./contactForm.css";
 
 function ContactForm() {
+
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const messageRef = useRef();
+
+    const [messageSent, setMessageSent] = useState(false);
+    const [misingFields, setMissingFields] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        let name = nameRef.current.value;
+        let email = emailRef.current.value;
+        let message = messageRef.current.value;
+
+        if (name && email && message) {
+            setMissingFields(false);
+            console.log("Form submitting...");
+            setMessageSent(true);
+            setTimeout(() => {
+                window.location.reload(false);
+            }, 3000);
+
+        } else {
+            console.log("Empty fields...")
+            setMissingFields(true);
+        }
+
+    }
+
     return (
-        <form className="uk-form-stacked uk-position-relative" uk-height-viewport="expand: true">
+        <form className="uk-form-stacked uk-position-relative" uk-height-viewport="expand: true" onSubmit={handleSubmit}>
             <div className="uk-margin">
-                <label className="uk-form-label uk-text-muted">Name</label>
+                <label className="uk-form-label uk-text-muted" htmlFor="name">Name</label>
                 <div className="uk-form-controls">
-                    <input className="uk-input" id="name" type="text" placeholder="John Doe" />
+                    <input className="uk-input" id="name" type="text" placeholder="John Doe" ref={nameRef} />
                 </div>
             </div>
             <div className="uk-margin">
-                <label className="uk-form-label uk-text-muted">Email</label>
+                <label className="uk-form-label uk-text-muted" htmlFor="email">Email</label>
                 <div className="uk-form-controls">
-                    <input className="uk-input" id="email" type="text" placeholder="johndoe@email.com" />
+                    <input className="uk-input" id="email" type="text" placeholder="johndoe@email.com" ref={emailRef} />
                 </div>
             </div>
             <div className="uk-margin">
-                <label className="uk-form-label uk-text-muted">Email</label>
+                <label className="uk-form-label uk-text-muted" htmlFor="message">Message</label>
                 <div className="uk-form-controls">
                     <textarea className="uk-textarea" id="message" rows="5"
-                        placeholder="Your message..."></textarea>
+                        placeholder="Your message..." ref={messageRef} ></textarea>
                 </div>
+                {
+                    messageSent ? (
+                        <div className="success-alert-container uk-alert-success" uk-alert="true">
+                            <span className="uk-alert-close" uk-close="true"></span>
+                            <p>Your message was sent successfully!</p>
+                        </div>
+                    ) : ""
+                }
+                {
+                    misingFields ? (
+                        <div className="danger-alert-container uk-alert-danger" uk-alert="true">
+                            <span className="uk-alert-close" uk-close="true"></span>
+                            <p>Please fill in all the fields and try again.</p>
+                        </div>
+                    ) : ""
+                }
             </div>
-            <button id="submitBtn" className="uk-button" >Submit</button>
+            <button id="submitBtn" className="uk-button" type="submit">Submit</button>
         </form>
     );
 }
