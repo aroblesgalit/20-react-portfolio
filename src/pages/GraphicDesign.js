faedcdf9b823d7632c3dc35bcd102910dff78097
Alvin Galit <aroblesgalit@gmail.com>
2020-09-12 22:47:15 -0500

    Used the API for fetching graphic design projects

--- src/pages/GraphicDesign.js
+++ src/pages/GraphicDesign.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./pages.css";
 import GDProjSection from "../components/GDProjSection";
+import API from "../utils/API";
 
 export default function GraphicDesign() {
+    const [gdProjects, setGDProjects] = useState([]);
+
+    useEffect(() => {
+        API.getGDProjects()
+        .then(res => setGDProjects(res))
+        .catch(err => console.log(err));
+    }, [])
+
     return (
         <div className="container">
             <h2 className="uk-text-center">Graphic Design Portfolio</h2>
-            <GDProjSection />
+            {
+                gdProjects.map(project => (
+                    <GDProjSection
+                        key={project.title}
+                        title={project.title}
+                        type={project.type}
+                        image={project.image}
+                        link={project.link}
+                    />
+                ))
+            }
         </div>
     )
 }
