460e28145927c9975243d3336421f93a2764f5a3
Alvin Galit <aroblesgalit@gmail.com>
2020-09-12 21:39:17 -0500

    Created a graphic design project section component

---
+++ src/components/GDProjSection/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./gdProjSection.css";
+
+export default function GDProjSection() {
+    return (
+        <section>
+            Graphic Design Project One
+        </section>
+    )
+}
