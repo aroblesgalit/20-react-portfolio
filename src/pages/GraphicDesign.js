355df91489f16f4e055d3f757fa778d9246e82f9
Alvin Galit <aroblesgalit@gmail.com>
2020-09-12 21:35:56 -0500

    Created a page for my graphic design works

---
+++ src/pages/GraphicDesign.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./pages.css";
+
+export default function GraphicDesign() {
+    return (
+        <div className="container">
+            <h2>Graphic Design Portfolio</h2>
+            
+        </div>
+    )
+}
