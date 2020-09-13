3dee8b44ef0ae960b532b456df0ff571215e0520
Alvin Galit <aroblesgalit@gmail.com>
2020-09-12 21:51:12 -0500

    Fixed issue with section width

--- src/pages/GraphicDesign.js
+++ src/pages/GraphicDesign.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./pages.css";
+import GDProjSection from "../components/GDProjSection";
 
 export default function GraphicDesign() {
     return (
         <div className="container">
             <h2>Graphic Design Portfolio</h2>
-            
+            <GDProjSection />
         </div>
     )
 }
