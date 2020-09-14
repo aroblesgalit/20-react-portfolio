3db0a347c4bb4ae6f7952c211f9b5823a26e45f9
Alvin Galit <aroblesgalit@gmail.com>
2020-09-14 15:44:43 -0500

    Updated project info component to take props

--- src/components/ProjectInfo/index.js
+++ src/components/ProjectInfo/index.js
@@ -1,23 +1,18 @@
 import React from "react";
 import "./projectInfo.css";
 
-export default function ProjectInfo() {
+export default function ProjectInfo({ title, description, services }) {
     return (
         <div className="project-info uk-flex uk-flex-column uk-flex-around">
-            <h2 className="uk-text-center">Indigo Ball Fundraiser Event</h2>
+            <h2 className="uk-text-center">{title}</h2>
             <div className="uk-flex uk-flex-column uk-width-100">
                 <div className="uk-flex uk-margin-bottom uk-width-100">
                     <h3>ABOUT THE PROJECT</h3>
-                    <p>
-                        I was tasked to design the LGBTQ Center OC's (a gay and lesbian
-                        non-profit organization) logo for their annual Indigo Ball black-tie
-                        fundraiser event. The logo was to be classy and clever that would
-                        work well on print and on the website.
-                    </p>
+                    <p>{description}</p>
                 </div>
                 <div className="uk-flex uk-margin-bottom uk-width-100">
                     <h3>SERVICES</h3>
-                    <p>Logo Design, Print Design</p>
+                    <p>{services}</p>
                 </div>
             </div>
         </div>
--- src/pages/IndigoBall.js
+++ src/pages/IndigoBall.js
@@ -8,7 +8,14 @@ export default function IndigoBall() {
     return (
         <div className="container">
             <Splash imgUrl={indigoBallSplash} />
-            <ProjectInfo />
+            <ProjectInfo 
+                title="Indigo Ball Fundraiser Event" 
+                description="I was tasked to design the LGBTQ Center OC's (a gay and lesbian
+                    non-profit organization) logo for their annual Indigo Ball black-tie
+                    fundraiser event. The logo was to be classy and clever that would
+                    work well on print and on the website."
+                services="Logo Design, Print Design"
+            />
         </div>
     )
 }
