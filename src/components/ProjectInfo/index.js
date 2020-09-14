721f704c9d7b205b24d81ef0cdc5a23738dc70d6
Alvin Galit <aroblesgalit@gmail.com>
2020-09-14 15:26:59 -0500

    Created project info component

---
+++ src/components/ProjectInfo/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function ProjectInfo() {
+    return (
+        <div>
+            <h2>Indigo Ball Fundraiser Event</h2>
+            <div>
+                <div>
+                    <h3>ABOUT THE PROJECT</h3>
+                    <p>
+                        I was tasked to design the LGBTQ Center OC's (a gay and lesbian
+                        non-profit organization) logo for their annual Indigo Ball black-tie
+                        fundraiser event. The logo was to be classy and clever that would
+                        work well on print and on the website.
+                </p>
+                </div>
+                <div>
+                    <h3>SERVICES</h3>
+                    <p>Logo Design, Print Design</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/IndigoBall.js
+++ src/pages/IndigoBall.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./pages.css";
 import Splash from "../components/Splash";
 import indigoBallSplash from "../assets/images/indigoBall-splash.jpg";
+import ProjectInfo from "../components/ProjectInfo";
 
 export default function IndigoBall() {
     return (
         <div className="container">
             <Splash imgUrl={indigoBallSplash} />
+            <ProjectInfo />
         </div>
     )
 }
