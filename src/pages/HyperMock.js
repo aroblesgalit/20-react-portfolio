5e776ff1214b5dbe1d5144d392a4f1695906b553
Alvin Galit <aroblesgalit@gmail.com>
2020-09-15 15:27:35 -0500

    Added vectors content to hypermock project page

--- src/pages/HyperMock.js
+++ src/pages/HyperMock.js
@@ -7,6 +7,7 @@ import ProjectContent from "../components/ProjectContent";
 import hyperMockSketch1 from "../assets/images/hyperMock-sketch-1.jpg";
 import hyperMockSketch2 from "../assets/images/hyperMock-sketch-2.jpg";
 import hyperMockSketch3 from "../assets/images/hyperMock-sketch-3.jpg";
+import hyperMockVectorize from "../assets/images/hyperMock-vectorize.jpg";
 
 export default function HyperMock() {
 
@@ -45,7 +46,14 @@ export default function HyperMock() {
             url: hyperMockSketch3,
             title: "Logo sketches"
         }
-    ]
+    ];
+
+    const vectors = [
+        {
+            url: hyperMockVectorize,
+            title: "Vectorized sketches of 3 logo ideas."
+        }
+    ];
 
     return (
         <div className="container">
@@ -73,6 +81,12 @@ export default function HyperMock() {
                 description="Based on the words I have brainstormed, I then sketched out
                     some ideas."
                 images={sketches}
+            />
+            <ProjectContent
+                type="other"
+                heading="3 vectors"
+                description="Then I used Illustrator to vectorize the best ones."
+                images={vectors}
             />
         </div>
     )
