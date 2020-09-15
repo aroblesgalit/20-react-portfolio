3bbd138db6e5b10f6edf42bc5980d79c153370ea
Alvin Galit <aroblesgalit@gmail.com>
2020-09-15 14:57:40 -0500

    Created page for HyperMock project

---
+++ src/pages/HyperMock.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./pages.css";
+
+export default function HyperMock() {
+    return (
+        <div className="container">
+            HyperMock
+        </div>
+    )
+}
