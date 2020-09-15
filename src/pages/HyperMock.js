93938297182bc4f3c7f10f34f8663ff024b48d09
Alvin Galit <aroblesgalit@gmail.com>
2020-09-15 15:13:26 -0500

    Added project content including brainstorm component to hypermock project

--- src/pages/HyperMock.js
+++ src/pages/HyperMock.js
@@ -3,8 +3,32 @@ import "./pages.css";
 import Splash from "../components/Splash";
 import hyperMockSplash from "../assets/images/hyperMock-splash.jpg";
 import ProjectInfo from "../components/ProjectInfo";
+import ProjectContent from "../components/ProjectContent";
 
 export default function HyperMock() {
+
+    // Brainstorm words
+    const words = [
+        {
+            word: "authoring tool",
+            graphics: ["gears", "wrench", "pencil"]
+        },
+        {
+            word: "ar/vr",
+            graphics: ["3d objects", "vr headset", "monitor"]
+        }
+    ];
+
+    const combinations = [
+        ["gears", "vr headset"],
+        ["pencil", "vr headset"],
+        ["pencil", "gears", "vr headset"],
+        ["monitor", "vr headset"],
+        ["monitor", "gears", "vr headset"],
+        ["3d object", "vr headset"],
+        ["3d object", "gears", "vr headset"]
+    ];
+
     return (
         <div className="container">
             <Splash imgUrl={hyperMockSplash} />
@@ -16,6 +40,14 @@ export default function HyperMock() {
                     reality (AR/VR) content. I was also tasked to design the user interface
                     (UI) for the product's desktop and AR/VR applications."
                 services="Logo Design, UI Design"
+            />
+            <ProjectContent
+                type="brainstorm"
+                heading="1 brainstorm"
+                description="Before designing the logo, I started with brainstorming words
+                    that describe HyperMock."
+                words={words}
+                combinations={combinations}
             />
         </div>
     )
