6250ab25ffef25b0add7738c6952672085a2e4f4
Alvin Galit <aroblesgalit@gmail.com>
2020-09-15 17:52:10 -0500

    Added splash component to the other logos page

--- src/pages/OtherLogos.js
+++ src/pages/OtherLogos.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./pages.css";
+import Splash from "../components/Splash";
+import otherLogosSplash from "../assets/images/otherLogos-splash.jpg";
 
 export default function OtherLogos() {
     return (
         <div className="container">
-            Other Logos
+            <Splash imgUrl={otherLogosSplash} />
         </div>
     )
 }
