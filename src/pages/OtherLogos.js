4da0a5bf7a6b5318492b29274ef61413b2adbb27
Alvin Galit <aroblesgalit@gmail.com>
2020-09-15 17:47:47 -0500

    Created Other Logos project page

---
+++ src/pages/OtherLogos.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./pages.css";
+
+export default function OtherLogos() {
+    return (
+        <div className="container">
+            Other Logos
+        </div>
+    )
+}
