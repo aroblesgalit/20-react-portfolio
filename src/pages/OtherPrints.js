731a4744e2f9b05b8d2e0b856a7e4a429f300a86
Alvin Galit <aroblesgalit@gmail.com>
2020-09-16 19:47:09 -0500

    Created a page for OtherPrints

---
+++ src/pages/OtherPrints.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./pages.css";
+
+export default function OtherPrints() {
+    return (
+        <div className="container">
+            OtherPrints
+        </div>
+    )
+}
