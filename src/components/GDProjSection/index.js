f43b9c3b7915d2c6f991bf1303150832c9645c9d
Alvin Galit <aroblesgalit@gmail.com>
2020-09-16 19:34:41 -0500

    Updated button on GDProjSection component

--- src/components/GDProjSection/index.js
+++ src/components/GDProjSection/index.js
@@ -1,22 +1,21 @@
 import React from "react";
 import "./gdProjSection.css";
+import { Link } from "react-router-dom";
 
 export default function GDProjSection({ title, type, image, link }) {
     return (
         <section className="uk-flex uk-position-relative">
             <img src={image} alt={title} />
-            <div className="uk-light uk-position-absolute">
-                <p className="uk-text-small uk-margin-remove">
+            <div className="uk-position-absolute">
+                <p className="uk-light uk-text-small uk-margin-remove">
                     {type.join(", ")}
                 </p>
-                <h3 className="uk-margin-remove">{title}</h3>
-                <button 
-                    type="button" 
-                    className="uk-button uk-button-default uk-margin"
-                    onClick={() => window.location.href=link}
-                >
-                    See project
-                </button>
+                <h3 className="uk-light uk-margin-remove-top">{title}</h3>
+                <Link to={link}>
+                    <button className="ctaBtn uk-button uk-button-default uk-margin">
+                        See project
+                    </button>
+                </Link>
             </div>
         </section>
     )
