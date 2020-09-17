945c3d6db2c9c8115cdfcc0fc0734aa217c12a18
Alvin Galit <aroblesgalit@gmail.com>
2020-09-16 19:51:00 -0500

    Added splash component in OtherPrints page

--- src/pages/OtherPrints.js
+++ src/pages/OtherPrints.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./pages.css";
+import Splash from "../components/Splash";
+import otherPrintsSplash from "../assets/images/otherPrints-splash.jpg";
 
 export default function OtherPrints() {
     return (
         <div className="container">
-            OtherPrints
+            <Splash imgUrl={otherPrintsSplash} />
         </div>
     )
 }
