923872ffef2e34a82647466abc9b54265e8be071
Alvin Galit <aroblesgalit@gmail.com>
2020-09-16 19:53:22 -0500

    Added project info component to OtherPrints page

--- src/pages/OtherPrints.js
+++ src/pages/OtherPrints.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./pages.css";
 import Splash from "../components/Splash";
 import otherPrintsSplash from "../assets/images/otherPrints-splash.jpg";
+import ProjectInfo from "../components/ProjectInfo";
 
 export default function OtherPrints() {
     return (
         <div className="container">
             <Splash imgUrl={otherPrintsSplash} />
+            <ProjectInfo
+                title="Other Print Designs"
+                description="The following are various prints I designed in the last four years."
+                services="Print Designs"
+            />
         </div>
     )
 }
