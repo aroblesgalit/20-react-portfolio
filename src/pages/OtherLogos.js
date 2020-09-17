3ccb1b3c6cb5ce4368fc17a2385a22afe370ac21
Alvin Galit <aroblesgalit@gmail.com>
2020-09-16 19:11:49 -0500

    Added professional mixer series logo to OtherLogos page

--- src/pages/OtherLogos.js
+++ src/pages/OtherLogos.js
@@ -11,6 +11,7 @@ import careosityLogo from "../assets/images/careosity_logo.png";
 import transPrideLogo from "../assets/images/transPride_logo.png";
 import sipsAndGigglesLogo from "../assets/images/sipsAndGiggles_logo.png";
 import lifeGamesLogo from "../assets/images/lifeGames_logo.png";
+import professionalMixerLogo from "../assets/images/professionalMixer_logo.png";
 
 export default function OtherLogos() {
 
@@ -56,6 +57,12 @@ export default function OtherLogos() {
             alt: "Logo for a mobile app called LifeGames.",
             shape: "vertical",
             background: "#a41c43"
+        },
+        {
+            url: professionalMixerLogo,
+            alt: "Logo for a mixer event called Professional Mixer Series.",
+            shape: "horizontal",
+            background: "#f4f4f4"
         }
     ];
 
