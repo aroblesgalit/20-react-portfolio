d6f8a65c88f41a85b5a685f70dfa11371c8a9b0a
Alvin Galit <aroblesgalit@gmail.com>
2020-09-17 21:38:28 -0500

    Added logic for linking the graphic design nav

--- src/components/GDNav/index.js
+++ src/components/GDNav/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./gdNav.css";
+import { Link } from "react-router-dom";
 
 export default function GDNav() {
+
+    const currentPath = window.location.pathname;
+
     return (
-        <div className="gd-nav uk-position-fixed">
-            Nav
+        <div className="gd-nav uk-position-fixed uk-flex uk-flex-between">
+            <Link to={currentPath === "/other-prints" ? "/other-logos" : currentPath === "/other-logos" ? "/hypermock" : "/indigo-ball"}>
+                <span
+                    uk-icon="chevron-left"
+                    className={`gd-nav-left ${currentPath === "/indigo-ball" ? "hidden" : ""}`}
+                />
+            </Link>
+            <Link to={currentPath === "/indigo-ball" ? "/hypermock" : currentPath === "/hypermock" ? "/other-logos" : "/other-prints"}>
+                <span uk-icon="chevron-right"
+                    className={`gd-nav-right ${currentPath === "/other-prints" ? "hidden" : ""}`}
+                />
+            </Link>
         </div>
     )
 }
