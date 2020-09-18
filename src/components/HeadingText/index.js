f8c8bbd19a80e7f1a623d58742f24db3f6902b53
Alvin Galit <aroblesgalit@gmail.com>
2020-09-18 16:54:00 -0500

    Added animation to subheading and resume button

--- src/components/HeadingText/index.js
+++ src/components/HeadingText/index.js
@@ -35,6 +35,32 @@ function HeadingText() {
         delay: 250
     });
 
+    const propsSub = useSpring({
+        from: {
+            opacity: 0
+        },
+        to: {
+            opacity: 1
+        },
+        config: {
+            duration: 500
+        },
+        delay: 750
+    });
+
+    const propsResume = useSpring({
+        from: {
+            opacity: 0
+        },
+        to: {
+            opacity: 1
+        },
+        config: {
+            duration: 250
+        },
+        delay: 1000
+    });
+
     return (
         <div>
             <div className="uk-tile headingText uk-text-left" uk-height-viewport="expand: true">
@@ -42,9 +68,9 @@ function HeadingText() {
                     <animated.div style={propsHello} className="hello">Hello! I'm</animated.div>
                     <animated.div style={propsName} className="name">Alvin<span>.</span></animated.div>
                 </div>
-                <p className="subHeading">Web Developer and Graphic Designer</p>
+                <animated.p style={propsSub} className="subHeading">Web Developer and Graphic Designer</animated.p>
                 <Link to={resumePdf} target="_blank">
-                    <button className="ctaBtn uk-button uk-button-default uk-margin">Resume</button>
+                    <animated.button style={propsResume} className="ctaBtn uk-button uk-button-default uk-margin">Resume</animated.button>
                 </Link>
             </div>
         </div>
