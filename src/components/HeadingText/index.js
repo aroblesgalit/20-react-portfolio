aa855f773df5d5cdd1ad1a791ba1feb6947428a9
Alvin Galit <aroblesgalit@gmail.com>
2020-09-18 16:26:32 -0500

    Rearranged greeting text

--- src/components/HeadingText/index.js
+++ src/components/HeadingText/index.js
@@ -1,13 +1,17 @@
 import React from "react";
-import "./headingText.css";
 import { Link } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
+import "./headingText.css";
 import resumePdf from "../../assets/images/AlvinGalit_Resume_July2020.pdf";
 
 function HeadingText() {
     return (
         <div>
             <div className="uk-tile headingText uk-text-left" uk-height-viewport="expand: true">
-                <div className="greeting">Hello! I'm <span>Alvin</span>.</div>
+                <div className="greeting">
+                    <div className="hello">Hello! I'm</div>
+                    <div className="name">Alvin<span>.</span></div>
+                </div>
                 <p className="subHeading">Web Developer and Graphic Designer</p>
                 <Link to={resumePdf} target="_blank">
                     <button className="ctaBtn uk-button uk-button-default uk-margin">Resume</button>
