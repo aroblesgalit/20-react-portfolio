fc839121a15fee7f70d2bf37a0fad1dd67391b47
Alvin Galit <aroblesgalit@gmail.com>
2020-09-18 16:50:08 -0500

    Used useSpring to animate my greeting text

--- src/components/HeadingText/index.js
+++ src/components/HeadingText/index.js
@@ -5,12 +5,42 @@ import "./headingText.css";
 import resumePdf from "../../assets/images/AlvinGalit_Resume_July2020.pdf";
 
 function HeadingText() {
+
+    const propsHello = useSpring({
+        from: {
+            opacity: 0,
+            transform: "translateY(-80px)"
+        },
+        to: {
+            opacity: 1,
+            transform: "translateY(0)"
+        },
+        config: {
+            duration: 750
+        }
+    });
+
+    const propsName = useSpring({
+        from: {
+            opacity: 0,
+            transform: "translateY(-60px)"
+        },
+        to: {
+            opacity: 1,
+            transform: "translateY(0)"
+        },
+        config: {
+            duration: 500
+        },
+        delay: 250
+    });
+
     return (
         <div>
             <div className="uk-tile headingText uk-text-left" uk-height-viewport="expand: true">
                 <div className="greeting">
-                    <div className="hello">Hello! I'm</div>
-                    <div className="name">Alvin<span>.</span></div>
+                    <animated.div style={propsHello} className="hello">Hello! I'm</animated.div>
+                    <animated.div style={propsName} className="name">Alvin<span>.</span></animated.div>
                 </div>
                 <p className="subHeading">Web Developer and Graphic Designer</p>
                 <Link to={resumePdf} target="_blank">
