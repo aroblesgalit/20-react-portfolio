52acedf30f04c47c8725475b0010a796458c3787
Alvin Galit <aroblesgalit@gmail.com>
2020-10-08 22:05:15 -0500

    Created component for post

---
+++ src/components/Post/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./post.css";
+
+export default function Post() {
+    return (
+        <div className="postCard uk-card uk-card-default">
+            <div className="uk-card-body">
+                <p>10/8/2020 9:58PM</p>
+                <h3 className="uk-card-title uk-text-truncate">LeetCode Challenge</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <a className="repoLink" href="/">READ MORE</a>
+            </div>
+        </div>
+    )
+}
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./pages.css";
+import Post from "../components/Post";
 
 export default function Posts() {
     return (
         <div className="container">
             <div className="posts-wrapper">
                 <h2 className="uk-text-center">Posts</h2>
+                <div className="uk-flex uk-flex-column uk-flex-middle">
+                    <Post />
+                </div>
             </div>
         </div>
     )
