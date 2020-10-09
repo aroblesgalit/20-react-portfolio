89905648ee3d3f3545ee1fea25e1b690483ba7c4
Alvin Galit <aroblesgalit@gmail.com>
2020-10-08 22:26:57 -0500

    Updated Post to use secondary button

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./post.css";
+import ButtonSecondary from "../ButtonSecondary";
 
 export default function Post() {
     return (
-        <div className="postCard uk-card uk-card-default">
+        <div className="post-card uk-card uk-card-default">
             <div className="uk-card-body">
                 <p className="date">10/8/2020 9:58PM</p>
                 <h3 className="uk-card-title uk-text-truncate">LeetCode Challenge</h3>
-                <p>
+                <p className="post-copy uk-text-break">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
                     nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
@@ -15,7 +16,7 @@ export default function Post() {
                     fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
                     culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <a className="repoLink" href="/">READ MORE</a>
+                <ButtonSecondary text="read more" path="/" />
             </div>
         </div>
     )
