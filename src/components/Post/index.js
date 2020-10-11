f72f34db8a10ddcf5bf063d9ad62b507a6cf780f
Alvin Galit <aroblesgalit@gmail.com>
2020-10-10 23:29:39 -0500

    Updated Post component to accept props

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./post.css";
 
-export default function Post() {
+export default function Post({date, title, copy}) {
 
     const [readMore, setReadMore] = useState(false);
 
@@ -16,12 +16,10 @@ export default function Post() {
     return (
         <div className="post-card uk-card uk-card-default">
             <div className="uk-card-body">
-                <p className="date">10/8/2020 9:58PM</p>
-                <h3 className="uk-card-title uk-text-truncate">LeetCode Challenge</h3>
+    <p className="date">{date}</p>
+                <h3 className="uk-card-title uk-text-truncate">{title}</h3>
                 <p className={`post-copy ${readMore === false && "less"} uk-text-break`}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    {copy}
                 </p>
                 <span onClick={handleView}>{readMore ? "...see less." : "See more..."}</span>
             </div>
