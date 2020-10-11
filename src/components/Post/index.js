b8688f039c9205eb27faa46ac9e8525092039681
Alvin Galit <aroblesgalit@gmail.com>
2020-10-10 23:19:23 -0500

    Added logic for toggling view more and view less of post content

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./post.css";
 import ButtonSecondary from "../ButtonSecondary";
 
 export default function Post() {
+
+    const [readMore, setReadMore] = useState(false);
+
+    function handleMore() {
+        if (readMore) {
+            setReadMore(false);
+        } else {
+            setReadMore(true);
+        }
+    }
+
     return (
         <div className="post-card uk-card uk-card-default">
             <div className="uk-card-body">
                 <p className="date">10/8/2020 9:58PM</p>
                 <h3 className="uk-card-title uk-text-truncate">LeetCode Challenge</h3>
-                <p className="post-copy uk-text-break">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                <p className={`post-copy ${readMore === false && "less"} uk-text-break`}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                     nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
-                <ButtonSecondary text="read more" path="/" />
+                <span onClick={handleMore}>{readMore ? "See less..." : "See more..."}</span>
             </div>
         </div>
     )
