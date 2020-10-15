23b3f0cdbdce9472660dd1144fa069d6bde2fafc
Alvin Galit <aroblesgalit@gmail.com>
2020-10-14 20:32:32 -0500

    Updated contact page with page title component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./pages.css";
 import ContactInfo from "../components/ContactInfo";
 import ContactForm from "../components/ContactForm";
+import PageTitle from "../components/PageTitle";
 
 function Contact() {
     return (
         <div className="container">
             <div className="contact-wrapper">
-                <h2 className="uk-text-center">Contact</h2>
+                <PageTitle text="Contact" color="light" />
                 <div className="contactWrapper uk-child-width-1-2@m" uk-grid="true">
                     <ContactInfo />
                     <ContactForm />
