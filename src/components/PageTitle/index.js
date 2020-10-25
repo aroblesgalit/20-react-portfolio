6ae968053ec829637671fc73e57f106d2c87d15a
Alvin Galit <aroblesgalit@gmail.com>
2020-10-25 13:19:54 -0500

    Positioned page title absolute to top right corner on mobile

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -3,6 +3,6 @@ import "./pageTitle.css";
 
 export default function PageTitle({ text, color }) {
     return (
-        <h2 className={`uk-text-center ${color}`}>{text}</h2>
+        <h2 className={`uk-text-center ${color} ${(text === 'Portfolio' || text === 'Graphic Design' || text === 'Posts' || text === 'Contact') && 'mobile-page-title' }`}>{text}</h2>
     )
 }
