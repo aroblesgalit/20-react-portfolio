ec17d9ed70fb369961bc8b142dd1a8518f7b4841
Alvin Galit <aroblesgalit@gmail.com>
2021-12-09 22:31:55 -0700

    Added a new post

--- src/utils/API.js
+++ src/utils/API.js
@@ -57,6 +57,13 @@ import js30WhackAMole from '../assets/images/js30_whackAMole.png'
 
 const posts = [
   {
+    id: 49,
+    date: '12/09/21 10:31PM',
+    title: '1 Year',
+    copy:
+      'Today marks my one year at Ibotta. It has been an amazing time learning and growing at a company that cares so much in every aspect. I can really see myself continuing my career at Ibotta for many more years.'
+  },
+  {
     id: 48,
     date: '6/24/21 9:51PM',
     title: 'Mid-Year Review',
