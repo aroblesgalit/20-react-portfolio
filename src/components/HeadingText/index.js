3fb2f125f386918450f18ee75293e77b55877852
Alvin Galit <aroblesgalit@gmail.com>
2022-05-08 21:42:54 -0600

    Changed heading text role to Frontend Web Developer

--- src/components/HeadingText/index.js
+++ src/components/HeadingText/index.js
@@ -1,83 +1,91 @@
-import React from "react";
-import { useSpring, animated } from "react-spring";
-import "./headingText.css";
-import resumePdf from "../../assets/images/AlvinGalit_Resume.pdf";
-import ButtonPrimary from "../ButtonPrimary";
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
+import './headingText.css'
+import resumePdf from '../../assets/images/AlvinGalit_Resume.pdf'
+import ButtonPrimary from '../ButtonPrimary'
 
-function HeadingText() {
+function HeadingText () {
+  const propsHello = useSpring({
+    from: {
+      opacity: 0,
+      transform: 'translateY(-80px)'
+    },
+    to: {
+      opacity: 1,
+      transform: 'translateY(0)'
+    },
+    config: {
+      duration: 750
+    }
+  })
 
-    const propsHello = useSpring({
-        from: {
-            opacity: 0,
-            transform: "translateY(-80px)"
-        },
-        to: {
-            opacity: 1,
-            transform: "translateY(0)"
-        },
-        config: {
-            duration: 750
-        }
-    });
+  const propsName = useSpring({
+    from: {
+      opacity: 0,
+      transform: 'translateY(-60px)'
+    },
+    to: {
+      opacity: 1,
+      transform: 'translateY(0)'
+    },
+    config: {
+      duration: 500
+    },
+    delay: 250
+  })
 
-    const propsName = useSpring({
-        from: {
-            opacity: 0,
-            transform: "translateY(-60px)"
-        },
-        to: {
-            opacity: 1,
-            transform: "translateY(0)"
-        },
-        config: {
-            duration: 500
-        },
-        delay: 250
-    });
+  const propsSub = useSpring({
+    from: {
+      opacity: 0
+    },
+    to: {
+      opacity: 1
+    },
+    config: {
+      duration: 500
+    },
+    delay: 750
+  })
 
-    const propsSub = useSpring({
-        from: {
-            opacity: 0
-        },
-        to: {
-            opacity: 1
-        },
-        config: {
-            duration: 500
-        },
-        delay: 750
-    });
+  const propsResume = useSpring({
+    from: {
+      opacity: 0
+    },
+    to: {
+      opacity: 1
+    },
+    config: {
+      duration: 250
+    },
+    delay: 1000
+  })
 
-    const propsResume = useSpring({
-        from: {
-            opacity: 0
-        },
-        to: {
-            opacity: 1
-        },
-        config: {
-            duration: 250
-        },
-        delay: 1000
-    });
-
-    return (
-        <div>
-            <div className="uk-tile headingText uk-text-left" uk-height-viewport="expand: true">
-                <div className="greeting">
-                    <animated.div style={propsHello} className="hello">Hello! I'm</animated.div>
-                    <animated.div style={propsName} className="name">Alvin<span>.</span></animated.div>
-                </div>
-                <animated.p style={propsSub} className="subHeading">Web Developer and Graphic Designer</animated.p>
-                <animated.div style={propsResume}>
-                    <ButtonPrimary text="Resume" path={resumePdf} newTab />
-                </animated.div>
-                {/* <Link to={resumePdf} target="_blank">
+  return (
+    <div>
+      <div
+        className='uk-tile headingText uk-text-left'
+        uk-height-viewport='expand: true'
+      >
+        <div className='greeting'>
+          <animated.div style={propsHello} className='hello'>
+            Hello! I'm
+          </animated.div>
+          <animated.div style={propsName} className='name'>
+            Alvin<span>.</span>
+          </animated.div>
+        </div>
+        <animated.p style={propsSub} className='subHeading'>
+          Frontend Web Developer
+        </animated.p>
+        <animated.div style={propsResume}>
+          <ButtonPrimary text='Resume' path={resumePdf} newTab />
+        </animated.div>
+        {/* <Link to={resumePdf} target="_blank">
                     <animated.button style={propsResume} className="ctaBtn uk-button uk-button-default uk-margin">Resume</animated.button>
                 </Link> */}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default HeadingText;
+export default HeadingText
