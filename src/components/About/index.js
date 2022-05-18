8ce7507b67d66ef237389eb439ec6be1b704b530
Alvin Galit <aroblesgalit@gmail.com>
2022-05-17 23:07:56 -0600

    Updated about copy

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,14 +15,15 @@ function About () {
           <PageTitle text='About' color='dark' />
           <p className='aboutMeText uk-text-justify uk-padding'>
             I'm a Frontend Web Developer from SoCal (Southern California). I
-            wasn't always a web developer. I was a Graphic Designer first
-            focused on print and User Interface designs. I was 2-3 years deep
-            when I picked up an interest in the coding world. I realized, not
-            only did I enjoy designing UIs, but I also had a lot of fun with the
-            development and building all the moving blocks. So at the beginning
-            of the pandemic in 2020, I decided to switch career and completed a
-            coding bootcamp from the University of Texas at Austin. I knew I
-            made the right choice when problem solving just clicks.
+            wasn't always a web developer. I was a Graphic Designer first with
+            focus on print and User Interface designs. I was 2-3 years deep when
+            I picked up an interest in the coding world. I realized, not only
+            did I enjoy designing UIs, but I also had a lot of fun with the
+            development process and building all the moving blocks to create
+            something functional. So at the beginning of the pandemic in 2020, I
+            decided to switch career and completed a coding bootcamp from the
+            University of Texas at Austin. I knew I made the right choice when
+            problem solving just clicks.
           </p>
           <p className='aboutMeText uk-text-justify uk-padding uk-padding-remove-top'>
             When I'm not coding, I enjoy drawing, working out, going on hikes,
